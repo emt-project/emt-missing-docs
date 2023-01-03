2bda197994452d3d717ed9a7d72a72e4899ec679
csae8092 <peter.andorfer@oeaw.ac.at>
2023-01-03 17:07:50 +0100

    script to create proper teis

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ unlike the other emt-collections in the collection 58705 the document structure 
 
 * `trans_out` -> holds manually exported TEI documents (2023-01-03)
 * `gesamtliste_enriched.csv` -> manually downloaded [Excel-Doc](https://oeawacat.sharepoint.com/:x:/r/sites/ACDH-CH_p_EMT_FamiliensacheHandlungsspielraeumeEleonoraMagdalena/_layouts/15/Doc.aspx?sourcedoc=%7B5390DC86-717C-4D74-8888-77736AD54D1E%7D&file=Gesamtliste%20korrigiert_FINAL_E.xlsx&action=default&mobileredirect=true) 
+
+## reproduce:
+
+* create manual transkribus TEI export and copy TEIs into `trans_out`
+* run `python scripts/create_templates.py` to create proper XML/TEIs
--- scripts/create_templates.py
+++ scripts/create_templates.py
@@ -2,13 +2,40 @@ import glob
 import jinja2
 import pandas as pd
 import os
+import shutil
 from acdh_tei_pyutils.tei import TeiReader
 from dateutil.parser import parse, ParserError
 from datetime import date
 import tqdm
 import lxml.etree as ET
+from collections import defaultdict
 
+shutil.rmtree('./data/editions')
 os.makedirs('./data/editions', exist_ok=True)
+
+files = sorted(glob.glob('./trans_out/*.xml'))
+facs = {}
+for x in files:
+    doc = TeiReader(x)
+    nsmap = doc.nsmap
+    pbs = doc.any_xpath('.//tei:pb')
+    d = defaultdict(list)
+    for ab in doc.any_xpath('.//tei:ab'):
+        facs_id = ab.attrib['facs']
+        page_id = "_".join(facs_id.split('_')[:2])
+        d[page_id].append(ET.tostring(ab, encoding='utf-8', pretty_print=True).decode('utf-8'))
+    for i, p in enumerate(doc.any_xpath('.//tei:surface')):
+        img_id = p.xpath("./tei:graphic[1]/@url", namespaces=nsmap)[0]
+        facs_id = p.attrib["{http://www.w3.org/XML/1998/namespace}id"]
+        pb_node = ET.tostring(pbs[i], encoding='utf-8', pretty_print=True).decode('utf-8')
+        ab_nodes = "".join(d.get(f"#{facs_id}"))
+        page = pb_node + "\n" + ab_nodes
+        facs[img_id] = {
+            "img_file_name": img_id,
+            "surface_xmlid": facs_id,
+            "surface_node": ET.tostring(p, encoding='utf-8', pretty_print=True).decode('utf-8').replace(' xmlns="http://www.tei-c.org/ns/1.0"', ''),
+            "page": page.replace(' xmlns="http://www.tei-c.org/ns/1.0"', '')
+        }
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./scripts/templates")
 templateEnv = jinja2.Environment(loader=templateLoader)
@@ -16,12 +43,34 @@ template = templateEnv.get_template('tei_template.xml')
 
 df = pd.read_csv('gesamtliste_enriched.csv')
 
-for gr, df in tqdm.tqdm(df.groupby('folder')):
+for gr, ndf in tqdm.tqdm(df.groupby('folder')):
     file_name = f"./data/editions/{gr}.xml"
+    if '44_8_' in gr or '45_8_' in gr:
+        payload = []
+        for i, nrow in ndf.iterrows():
+            img_name = nrow['Dateiname']
+            try:
+                payload.append(facs[img_name])
+            except KeyError:
+                continue
+        if len(payload) < 1:
+            continue
+        body = ""
+        facs_nodes = ""
+        for x in payload:
+            body = body + x['page']
+            facs_nodes = facs_nodes + x['surface_node']
+    else:
+        continue
     with open(file_name.lower(), 'w') as f:
-        row = df.iloc[0]
+        row = ndf.iloc[0]
+        
         item = {}
-        item['doc_id'] = 1
+        if '_blau_44' in gr:
+            doc_id = 565905
+        else:
+            doc_id = 402711
+        item['doc_id'] = doc_id
         item['col_id'] = "58705"
         item['settlement'] = "München"
         item['repositor'] = "Bayerisches Hauptstaatsarchiv"
@@ -99,19 +148,17 @@ for gr, df in tqdm.tqdm(df.groupby('folder')):
         item['place'] = row['Ort']
         item['writte_date'] = row['Datum']
         item['current_date'] = f"{date.today()}"
-        # facsimile = doc.any_xpath('.//tei:facsimile')[0]
-        # item["facsimile"] = ET.tostring(facsimile, encoding='utf-8', pretty_print=True).decode('utf-8')
-        # body = doc.any_xpath('.//tei:body')[0]
-        # body_string = ET.tostring(body, encoding='utf-8', pretty_print=True).decode('utf-8')
-        # body_string = body_string.replace('reason=""', '')
-        # body_string = body_string.replace('type=""', '')
-        # body_string = body_string.replace('<blackening>', '<seg type="blackening">')
-        # body_string = body_string.replace('</blackening>', '</seg>')
-        # body_string = body_string.replace('<comment>', '<seg type="comment">')
-        # body_string = body_string.replace('</comment>', '</seg>')
-        # body_string = body_string.replace('<blackening/>', '')
-        # body_string = body_string.replace('<comment/>', '')
-        # item["body_string"] = body_string
+        item["facsimile"] = facs_nodes
+        body_string = body
+        body_string = body_string.replace('reason=""', '')
+        body_string = body_string.replace('type=""', '')
+        body_string = body_string.replace('<blackening>', '<seg type="blackening">')
+        body_string = body_string.replace('</blackening>', '</seg>')
+        body_string = body_string.replace('<comment>', '<seg type="comment">')
+        body_string = body_string.replace('</comment>', '</seg>')
+        body_string = body_string.replace('<blackening/>', '')
+        body_string = body_string.replace('<comment/>', '')
+        item["body_string"] = body_string
         try:
             item['parsed_date'] = parse(item['writte_date'])
         except (ParserError, TypeError):
